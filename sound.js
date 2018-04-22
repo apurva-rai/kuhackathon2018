8d014bcd822f9e679559713d1c4c3db64fa823be
Apurva Rai <apurvarai99@gmail.com>
2018-04-22 04:30:30 -0500

    Tentative sound files

---
+++ sound.js
@@ -0,0 +1,21 @@
+
+var actualSound;
+/*var modulator;
+var maxFrequency=220;
+var minFrequency=0;
+var maxSound= 220;
+var minSound=-220;*/
+
+function setup() {
+    console.log("This is happening.")
+    actualSound=new p5.Oscillator;
+    actualSound.setType("sine");
+    actualSound.amp(1);
+    actualSound.freq(400);
+    actualSound.start();
+}
+
+/*function realAudio()
+{
+  carrier.amp() 
+}*/
